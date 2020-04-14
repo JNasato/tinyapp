caf0da94dd0b092223e69ae7c55020ea287e4000
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-14 00:11:22 +0000

    added new url page to post from form

--- express_server.js
+++ express_server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function(){
+  return Math.random().toString(36).substr(2, 6);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -17,13 +23,22 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new.ejs');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -35,3 +50,4 @@ app.get('/urls/:shortURL', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
